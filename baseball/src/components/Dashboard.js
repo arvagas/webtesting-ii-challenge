c6db90bd50206d79ca7dc4448bd06cb72d65db1a
Arvin Agas <arvagas@gmail.com>
2019-08-08 14:47:33 -0400

    implemented testing

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -4,10 +4,10 @@ const Dashboard = ({ ballsHandler, strikesHandler, foulsHandler, hitsHandler }) 
     return (
         <div>
             <h1>Dashboard</h1>
-            <button onClick={ballsHandler}>Ball</button>
-            <button onClick={strikesHandler}>Strike</button>
-            <button onClick={foulsHandler}>Foul</button>
-            <button onClick={hitsHandler}>Hit</button>
+            <button onClick={ballsHandler} className='ball-btn'>Ball</button>
+            <button onClick={strikesHandler} className='strike-btn'>Strike</button>
+            <button onClick={foulsHandler} className='foul-btn'>Foul</button>
+            <button onClick={hitsHandler} className='hit-btn'>Hit</button>
         </div>
     )
 }
