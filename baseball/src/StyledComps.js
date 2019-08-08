fe46c5b8f2c34dd24a857131be8e7400739009de
Arvin Agas <arvagas@gmail.com>
2019-08-08 17:43:41 -0400

    styled display to make room for more info

--- baseball/src/StyledComps.js
+++ baseball/src/StyledComps.js
@@ -4,4 +4,16 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    max-width: 1000px;
+    width: 100%;
+`
+
+export const InfoCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem 2rem;
+    p {
+        margin: 0.5rem 0;
+    }
 `
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
+import { InfoCont } from '../StyledComps'
+
 const Display = ({ inning, balls, strikes, fouls, hits, outs }) => {
     return (
         <>
             <h1>Display Board</h1>
-            <p>Inning: {inning}</p>
-            <p>Balls: {balls}</p>
-            <p>Strikes: {strikes}</p>
-            <p>Fouls: {fouls}</p>
-            <p>Hits: {hits}</p>
-            <p>Outs: {outs}</p>
+            <div style={{display:'flex', justifyContent:'space-around'}}>
+                <InfoCont>
+                    <p>Inning:</p>
+                    <p>{inning}</p>
+                </InfoCont>
+                <InfoCont>
+                    <p>Balls:</p>
+                    <p>{balls}</p>
+                </InfoCont>
+                <InfoCont>
+                    <p>Strikes:</p>
+                    <p>{strikes}</p>
+                </InfoCont>
+                <InfoCont>
+                    <p>Fouls:</p>
+                    <p>{fouls}</p>
+                </InfoCont>
+                <InfoCont>
+                    <p>Hits:</p>
+                    <p>{hits}</p>
+                </InfoCont>
+                <InfoCont>
+                    <p>Outs:</p>
+                    <p>{outs}</p>
+                </InfoCont>
+            </div>
         </>
     )
 }
